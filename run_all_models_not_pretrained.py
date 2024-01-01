f08278ffb690528036f52ab78b9df2d5ca80a6ff
Alen Nasic <44438435+nasica137@users.noreply.github.com>
2024-01-01 21:31:53 +0100

    Create run_all_models_not_pretrained.py

---
+++ run_all_models_not_pretrained.py
@@ -0,0 +1,32 @@
+import json
+import subprocess
+
+# Load the JSON data
+with open('finetune-config1.json', 'r') as json_file:
+    config = json.load(json_file)
+
+# Define variations for encoder_name and loss[name]
+#encoder_variations = ['mobilenet_v2', 'xception', 'resnet18', 'resnet34', 'resnet50', 'vgg16']
+encoder_variations = ['resnet50', 'vgg16']
+loss_variations = ['DiceLoss', 'DiceCELoss', 'DiceFocalLoss']
+
+# Loop through each combination of encoder_name and loss[name]
+for encoder_name in encoder_variations:
+    for loss_name in loss_variations:
+        # Update the values in the config
+        config['model']['encoder_name'] = encoder_name
+        config['loss']['name'] = loss_name
+
+        # Construct the output directory using updated values
+        output_directory = config["output_directory"].format(**config)
+
+        # Write the updated config to a temporary JSON file
+        with open('temp_config3.json', 'w') as temp_file:
+            json.dump(config, temp_file)
+
+        # Execute the Python file with subprocess module
+        subprocess.run(['python', 'train_not_pretrained.py', '--config', 'temp_config3.json'])
+
+# After looping through all combinations, you might want to delete the temporary config file
+import os
+os.remove('temp_config3.json')
